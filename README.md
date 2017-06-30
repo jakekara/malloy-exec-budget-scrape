7fb45bcf0e58ac028345f5859e63a5edff2cbfb2
Jake Kara <jake@jakekara.com>
2017-06-30 15:06:19 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Scrape of agency budgents in governor's executive order
+
+This PDF has lots of tables:
+
+So, I wanted CSVs.
+
+I asked for them.
+
+I didn't get them.
+
+I ran the Section B pages of the PDF through Comet Docs to get one giant
+[CSV file](data/Just Section B - Sheet2.csv)
+
+I wrote [scrape_it.py](scrape_it.py) to walk through the CSV file and pull
+out the parts that are actually tables, store them in the [output](output)
+dir and ignore the rest.
+
+I can't vouch for the complete accuracy of these scrapes.
---
+++ scrape_it.py
@@ -0,0 +1,53 @@
+#
+# Process gross budget file line-by-line
+#
+import pandas as pd
+
+FNAME = "data/Just Section B - Sheet2.csv"
+
+in_a_table = False
+table_title = None
+start_of_table = None
+end_of_table = None
+
+lines = open(FNAME,"r").readlines()
+
+
+clean_line = lambda x: x.strip().upper()
+
+def start_table(name):
+    global table_title
+    in_a_table = False
+    table_title = name
+
+for i in range(len(lines)):
+# for i in range(20):
+    
+    line = lines[i]
+    if clean_line(line).startswith("AGENCY SUMMARY"):
+        start_table(lines[i - 2].replace(",","").strip())
+
+    # print line
+    if clean_line(line).startswith(",FY 2017,FY 2018,FY 2018"):
+        # print "\tFound table"
+        start_of_table = i
+        in_a_table = True
+
+
+    if clean_line(line).startswith("TOTAL - ALL FUNDS"):
+
+        if in_a_table is not True:
+            print "ERROR: FOUND ENDING WITHOUT BEGINNING"
+            exit()
+        
+        # print "\tFound end of table"
+        end_of_table = i
+        in_a_table = False
+        csv_str =  "".join(lines[start_of_table:end_of_table + 1])
+
+        open("TMP.csv","w").write(csv_str);
+
+        pd.read_csv("TMP.csv",skiprows=1).to_csv("output/" + table_title + ".csv",
+                                                 index=False)
+
+    
